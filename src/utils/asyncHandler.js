ad354e278a255eec22e046ddeb9e46aedf6c629b
17rishu <17singhrishabh@gmail.com>
2024-12-06 02:04:44 +0530

    utils code config

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,36 @@
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).reject((err) => next(err))
+    }
+}
+
+
+
+
+
+
+
+
+
+
+export { asyncHandler }
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => { () => {} }
+// const asyncHandler = (func) => () => {} 
+// const asyncHandler = (func) => async () => {}
+
+// step by step explained
+
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//     try {
+//         await func(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 404).json({
+//             message: error.message,
+//             success: false
+//         })
+//     }
+// }
