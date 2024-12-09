38ba58827dc85af6898184c0c76e787c7d3bc483
17rishu <17singhrishabh@gmail.com>
2024-12-09 18:07:48 +0530

    file upload setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -56,33 +56,33 @@ userSchema.pre("save", async function (next) {
   next();
 });
 
-userSchema.methods.isPasswordCorrect = async function(password){
-    return await bcrypt.compare('password', this.password)
-}
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare("password", this.password);
+};
 
-userSchema.methods.generateAccessToken = function(){
-    return jwt.sign(
-      {
-        _id: this._id,
-        email: this.email,
-        username: this.username,
-        fullname: this.fullname,
-      },
-      proces.env.ACCESS_TOKEN_SECRET,
-      {
-        expiresIn: process.env.ACCESS_TOKEN_EXPIRY
-      }
-    );   
-}
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
+    },
+    proces.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    }
+  );
+};
 
 userSchema.methods.generateRefreshToken = function () {
   return jwt.sign(
     {
-      _id: this._id
+      _id: this._id,
     },
     proces.env.REFRESH_TOKEN_SECRET,
     {
-      expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
     }
   );
 };
